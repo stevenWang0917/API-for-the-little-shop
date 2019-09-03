a7d76b2daaa398104a35cf1af8aa1f87b1b07779
Steven <wang.ruihai.toronto@gmail.com>
2019-09-03 12:30:23 -0400

    modify credentials

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const APP_SECRET = "myappecret";
 
-const USERNAME = 'admin', PASSWORD = 'secret';
+const USERNAME = 'admin', PASSWORD = 'admin';
 
 const anonOps = [
     {method: "GET", urls: ["/api/products", "/api/categories"]},
